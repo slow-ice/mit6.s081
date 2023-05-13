6d171145955b3d87dc81c92b3f38c98c74c48431
slowice <1357449044@qq.com>
2023-05-14 02:19:10 +0800

    complete first two tasks

---
+++ user/sleep.c
@@ -0,0 +1,13 @@
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "user.h"
+
+int main(char argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: sleep [time]\n");
+        exit(1);
+    }
+    
+    sleep(atoi(argv[1]));
+    exit(0);
+}
